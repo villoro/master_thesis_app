a31c19d6f9f0dc4f8b8c3d4768a87d252d48edcd
Arnau Villoro <villoro7@gmail.com>
2015-01-19 18:40:07 +0100

    Integracio amb MPAndroid Chart (https://github.com/PhilJay/MPAndroidChart)

---
+++ app/src/main/java/com/villoro/expensor_grafics/Graph.java
@@ -0,0 +1,70 @@
+package com.villoro.expensor_grafics;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
+
+public class Graph extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+
+        LineChart lineChart = (LineChart) findViewById(R.id.chart);
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < 4; i++) {
+            xVals.add((i) + "");
+        }
+
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+        yVals.add(new Entry(1, 1));
+        yVals.add(new Entry(4, 1));
+        yVals.add(new Entry(3, 1));
+        yVals.add(new Entry(2, 1));
+
+        LineDataSet set1 = new LineDataSet(yVals, "DataSet 1");
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+        dataSets.add(set1);
+
+        // create a data object with the datasets
+        LineData data = new LineData(xVals, dataSets);
+
+        // set data
+        lineChart.setData(data);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_graph, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
